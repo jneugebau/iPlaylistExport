3416fcf5d036794e1d817ede291d0b81885cb5b8
jn <joerg.neugebauer@me.com>
2019-05-15 20:08:00 +0200

    initial commit

---
+++ iPlaylistExport/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iPlaylistExport
+//
+//  Created by Joerg Neugebauer on 15.05.19.
+//  Copyright © 2019 Joerg Neugebauer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
